c2e7e668ba3d9aaaa5052654f16964e0fb1dd10a
Victor Grigoriu <vgrigoriu@gmail.com>
2014-03-02 14:26:50 +0200

    Read metadata from one file

--- listFiles/app.js
+++ listFiles/app.js
@@ -3,6 +3,7 @@
 
     var fs = require('fs');
     var path = require('path');
+    var musicmetadata = require('musicmetadata');
 
     var musicFolder = process.argv[2];
 
@@ -14,7 +15,11 @@
                 if (path.extname(fileOrFolderName) === '.flac') {
                     // TODO: get metadata
                     console.log(fileOrFolderName);
-                    process.exit();
+                    var parser = musicmetadata(fs.createReadStream(fileOrFolderName));
+                    parser.on('metadata', function(result) {
+                        console.log(result);
+                        process.exit();
+                    });
                 }
             } else if (stat.isDirectory()) {
                 list(fileOrFolderName);
